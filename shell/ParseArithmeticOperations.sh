8af2c9c00bd8fa6ff149b21737b63ba39ead103e
Timur Markunin <Timur_Markunin@epam.com>
2019-04-13 12:36:42 +0300

    added comment to parse arifmetic operations solution

--- shell/ParseArithmeticOperations.sh
+++ shell/ParseArithmeticOperations.sh
@@ -1,2 +1,9 @@
+#We provide you with expressions containing +,-,*,^, / and parenthesis. None of the numbers in the expression involved will exceed . 
+#Your task is to evaluate the expression and display the correct output rounding upto  decimal places.
+
+#Sample Input 1
+#5+50*3/20 + (19*2)/7
+
+
 read expression
 printf %.3f `echo $expression | bc -l`
